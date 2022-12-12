f05fd2fe45161289d86c634678d9d1e5dc3a794d
Laysa Santiago <laysa.santiago@minhaentrada.com.br>
2022-12-12 17:10:28 -0300

    Register completo

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom"
+import { AboveInformation } from "../../compoments/AboveInformartion"
+import { Input } from "../../compoments/InputBase"
+import { Button } from "../../compoments/PrimaryButton"
 
 export const Register = () => {
     return(
         <div className="py-8 flex flex-col items-center gap-4">
-            TESTE
+            <AboveInformation title="Por favor, digite suas informações para finalizar o cadastro"/>
+            
+            <form className="flex flex-col gap-4 items-center">
+                <Input label='E-mail'
+                    type='email'
+                />
+                <Input label='Telefone'
+                />
+                <Input label='Senha'
+                    type='password'/>
+                <Input label='Confirmar senha'
+                    type='password'
+                />
 
-            <Link to='/'>Voltar</Link>
+                <Button textButton='Cadastre-se'/>
+            </form>
+
+            <p>Já tem uma conta? <Link className='text-purpleBlue-900 hover:text-purpleBlue-700' to='/'>Faça o login</Link></p>
         </div>
     )
 }
